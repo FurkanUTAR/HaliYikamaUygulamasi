29c2dffd3048c641bf54a91e96c27e6461e4afe5
FurkanUTAR <137101949+FurkanUTAR@users.noreply.github.com>
2024-11-25 19:08:14 +0300

    Add files via upload

--- haliYikama/haliYikama/Properties/Resources.Designer.cs
+++ haliYikama/haliYikama/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace haliYikama.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap addOrder {
+            get {
+                object obj = ResourceManager.GetObject("addOrder", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Eye {
             get {
                 object obj = ResourceManager.GetObject("Eye", resourceCulture);
